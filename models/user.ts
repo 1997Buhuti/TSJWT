cbd51f563aede1e47a1dd14d8f10019b27f241d2
Manakal <manakal@randomsoftware.net>
2022-04-19 20:29:36 +0530

    feat: :zap: Added mongoose

---
+++ models/user.ts
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  fname: {
+    type: String,
+    required: true,
+    min: 6,
+    max: 255,
+  },
+
+  lname: {
+    type: String,
+    required: true,
+    min: 6,
+    max: 255,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    min: 6,
+    max: 255,
+  },
+
+  password: {
+    type: String,
+    required: true,
+    min: 6,
+    max: 255,
+  },
+
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
